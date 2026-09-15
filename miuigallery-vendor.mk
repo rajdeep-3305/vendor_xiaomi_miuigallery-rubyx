743a0e393fa38ae361900408380dc1a2d360c9e9
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2026-09-15 06:05:17 +0000

    miuigallery: Add micloud and security credential framework stubs
    * to satisfy runtime dependencies
    
    Change-Id: I9cff9d4e45ab9519b37f96f1a5078a2b094d7603
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- miuigallery-vendor.mk
+++ miuigallery-vendor.mk
@@ -2,13 +2,20 @@
 # Automatically generated file. DO NOT MODIFY
 #
 
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuigallery
+
 PRODUCT_PACKAGES += \
     MiuiGallery \
     MIMediaEditorGlobal \
     privapp-com.miui.gallery.xml \
     config-com.miui.gallery.xml \
-    default-com.miui.gallery.xml
+    default-com.miui.gallery.xml \
+    micloud-sdk.xml
+
 PRODUCT_COPY_FILES += \
+    vendor/xiaomi/miuigallery/proprietary/system/framework/micloud-sdk.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/micloud-sdk.jar \
+    vendor/xiaomi/miuigallery/proprietary/system/framework/security-device-credential-sdk.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/security-device-credential-sdk.jar \
     vendor/xiaomi/miuigallery/proprietary/product/etc/device_features/ruby.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/ruby.xml \
     vendor/xiaomi/miuigallery/proprietary/product/etc/device_features/rubypro.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/rubypro.xml \
     vendor/xiaomi/miuigallery/proprietary/product/etc/device_features/rubyplus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/rubyplus.xml
