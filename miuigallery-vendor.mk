df91df9adf6afef729bb0a665cf6e00a947c4938
rajdeep-3305 <rajdeepbiswas3305@gmail.com>
2026-09-15 06:05:17 +0000

    miuigallery: Add device feature XML configurations for rubyx variants
    * include device feature xml configs for ruby, rubypro, and rubyplus,
      and define copy rules to install them to product etc
    
    Change-Id: Ibaec672a390bc6b92cd8a7296454ff6fd50bab02
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- miuigallery-vendor.mk
+++ miuigallery-vendor.mk
@@ -8,3 +8,7 @@ PRODUCT_PACKAGES += \
     privapp-com.miui.gallery.xml \
     config-com.miui.gallery.xml \
     default-com.miui.gallery.xml
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/miuigallery/proprietary/product/etc/device_features/ruby.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/ruby.xml \
+    vendor/xiaomi/miuigallery/proprietary/product/etc/device_features/rubypro.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/rubypro.xml \
+    vendor/xiaomi/miuigallery/proprietary/product/etc/device_features/rubyplus.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/device_features/rubyplus.xml
